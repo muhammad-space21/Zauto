cde45cfc086eec8c23fc749e3d55bf93dea1f7e7
Daler Zokirov <zokirov.d@list.ru>
2020-06-02 11:18:38 +0500

    update

--- src/containers/Form/index.jsx
+++ src/containers/Form/index.jsx
@@ -6,19 +6,8 @@ import {
   Heading,
   FormStyled,
   TextWrapper,
-  Title,
   Input,
   ErrorMessage,
-  Select,
-  InputWrapper,
-  Wrapper,
-  OptionWhite,
-  OptionBlack,
-  OptionGrey,
-  OptionRed,
-  MessageOptional,
-  SelectLong,
-  Option,
   ButtonWrapper,
   Checkbox,
   LinkToPolicy,
@@ -33,15 +22,9 @@ class Form extends React.Component {
     super(props);
 
     this.state = {
-      firstname: '',
-      lastname: '',
-      patronymic: '',
+      fullname: '',
       phone: '',
-      color_car: '',
-      prepayment: '',
-      credit_term: '',
       submitted: false,
-      car: 'Maskvich'
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -56,23 +39,13 @@ class Form extends React.Component {
     e.preventDefault();
     // destructuring
     const {
-      firstname, 
-      lastname,
-      patronymic,
+      fullname,
       phone,
-      color_car,
-      prepayment,
-      credit_term
     } = this.state;
 
     const payload = {
-      firstname,
-      lastname,
-      patronymic,
+      fullname,
       phone,
-      color_car,
-      prepayment,
-      credit_term
     };
 
     axios({
@@ -88,21 +61,12 @@ class Form extends React.Component {
     this.setState({ submitted: true })
     // clear form
     if (
-      firstname 
-      && lastname 
-      && patronymic
-      && phone 
-      && color_car 
-      && prepayment 
-      && credit_term ) {
+      fullname 
+      && phone  
+    ) {
       this.setState({
-        firstname: '',
-        lastname: '',
-        patronymic: '',
+        fullname: '',
         phone: '',
-        color_car: '',
-        prepayment: '',
-        credit_term: '',
         submitted: false
       })
       console.log(this.state);
@@ -111,13 +75,8 @@ class Form extends React.Component {
 
   render() {
     const {
-      firstname, 
-      lastname, 
-      patronymic,
+      fullname, 
       phone,
-      color_car,
-      prepayment, 
-      credit_term,
       submitted
     } = this.state;
     return (
@@ -128,51 +87,18 @@ class Form extends React.Component {
           </TextWrapper>
           <Input
             type="text"
-            name="firstname"
+            name="fullname"
             placeholder="Имя"
-            value={firstname}
+            value={fullname}
             onChange={this.handleChange}
           />
           { 
-            submitted && !firstname &&
+            submitted && !fullname &&
             <ErrorMessage>Имя отсутствует!</ErrorMessage>
           }
           {
-            firstname !== '' && firstname.length < 3 ?
+            fullname !== '' && fullname.length < 3 ?
             <ErrorMessage>Имя должно быть более 3 символов!</ErrorMessage>
-            : null
-          }
-
-          <Input
-            type="text"
-            name="lastname"
-            placeholder="Фамилия"
-            value={lastname}
-            onChange={this.handleChange}
-          />
-          { 
-            submitted && !lastname && 
-            <ErrorMessage>Фамилия отсутствует!</ErrorMessage>
-          }
-          {
-            lastname !== '' && lastname.length < 3 ? 
-            <ErrorMessage>Фамилия должна быть более 5 символов!</ErrorMessage>
-            : null
-          }
-          <Input
-            type="text"
-            name="patronymic"
-            placeholder="Отчество"
-            value={patronymic}
-            onChange={this.handleChange}
-          />
-          { 
-            submitted && !patronymic && 
-            <ErrorMessage>Отчество отсутствует!</ErrorMessage>
-          }
-          {
-            patronymic !== '' && patronymic.length < 3 ? 
-            <ErrorMessage>Отчество должна быть более 5 символов!</ErrorMessage>
             : null
           }
           <InputTel
@@ -187,46 +113,8 @@ class Form extends React.Component {
             submitted && !phone &&
             <ErrorMessage>Номер телефона отсутствует!</ErrorMessage>
           }
-          <InputWrapper>
-            <Wrapper>
-              <Select value={color_car} onChange={this.handleChange} name="color_car" id="color_car">
-                <OptionWhite>Цвет авто</OptionWhite>
-                <OptionBlack value="Black">Black</OptionBlack>
-                <OptionRed value="Red">Red</OptionRed>
-                <OptionWhite value="White">White</OptionWhite>
-                <OptionGrey value="Grey">Grey</OptionGrey>
-              </Select>
-              { 
-                submitted && !color_car &&
-                <ErrorMessage>Выберите цвет!</ErrorMessage>
-              }
-            </Wrapper>
-            <Wrapper>
-              <Select value={prepayment} onChange={this.handleChange} name="prepayment" id="prepayment">
-                <Option>предоплата</Option>
-                <Option value="35%">35%</Option>
-                <Option value="50%">50%</Option>
-              </Select>
-              { 
-                submitted && !prepayment &&
-                <ErrorMessage>Выберите предоплату!</ErrorMessage>
-              }
-            </Wrapper>
-          </InputWrapper>
-          <SelectLong value={credit_term} onChange={this.handleChange} name="credit_term" id="credit_term">
-              <Option>Условия кредита</Option>
-              <Option value="12">12</Option>
-              <Option value="24">24</Option>
-              <Option value="36">36</Option>
-            </SelectLong>
-            { 
-              submitted && !credit_term &&
-              <ErrorMessage>Выберите срок кредитования!</ErrorMessage>
-            }
-            <InputWrapper>
               <Checkbox type="checkbox" id="terms" name="terms" />
               <LinkToPolicy to='/policy'>Agree to all terms</LinkToPolicy>            
-            </InputWrapper>
           <ButtonWrapper>
             <ButtonPrimary orderBtn>Отправить</ButtonPrimary>
           </ButtonWrapper>
